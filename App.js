c860ae96373abf28d180c4acc5ab5609be9ede5e
Drew Arocha <aarocha.aa@gmail.com>
2023-02-25 16:48:10 -0500

    added navigation

--- App.js
+++ App.js
@@ -2,16 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 import * as React from 'react';
+import {useState} from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import RecipesSCreen from './screens/RecipesScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import RecipesScreen from './screens/RecipesScreen';
+import SavedRecipesScreen from './screens/SavedRecipesScreen';
+
 export default function App() {
   const Stack = createStackNavigator();
+  const Tab = createBottomTabNavigator();
+  const [savedRecipes, SetSavedRecipes] = useState([]);
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Recipes" component={RecipesSCreen}/>
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Recipes" component={RecipesScreen}/>
+        <Tab.Screen name="Saved" component={SavedRecipesScreen}/>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
---
+++ screens/SavedRecipesScreen.js
@@ -0,0 +1,8 @@
+import {Text} from 'react-native';
+
+
+const SavedRecipesScreen = () =>{
+    <Text>Hello!</Text>
+}
+
+export default SavedRecipesScreen;
